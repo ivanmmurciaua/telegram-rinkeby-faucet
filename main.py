7dfb7fb87d56c2708be2255c61e9c2406b98061b
ivanmmurciaua <imm117@alu.ua.es>
2021-09-05 16:51:43 +0200

    Closing call to EscuelaCryptoES admins

--- main.py
+++ main.py
@@ -15,29 +15,29 @@ def help_command(update,context):
 
 def handle_message(update, context):
     print(update)
-    #########if update.message.chat['title'] == 'Jefazos EscuelaCryptoES':
-    text = str(update.message.text).lower()
-    response = R.sample_responses(text)
+    if update.message.chat['title'] == 'Jefazos EscuelaCryptoES':
+        text = str(update.message.text).lower()
+        response = R.sample_responses(text)
 
-    if len(response) == keys.AVERAGE_ETHEREUM_ADDRESS_LENGTH:
-        user = update.message.from_user['username']
-        user_address = response
-        
-        ####update.message.reply_text("Enviando 1 ETH de Rinkeby al jefazo " + user)
-        update.message.reply_text("Enviando 1 ETH de Rinkeby a " + user)
-        transaction_response = T.transaction(user, user_address)
-        if transaction_response:
-            update.message.reply_text("Transacción finalizada, disfruta de tu ETH :)")
+        if len(response) == keys.AVERAGE_ETHEREUM_ADDRESS_LENGTH:
+            user = update.message.from_user['username']
+            user_address = response
+            
+            update.message.reply_text("Enviando 1 ETH de Rinkeby a " + user)
+            transaction_response = T.transaction(user, user_address)
+            
+            if transaction_response:
+                update.message.reply_text("Transacción finalizada, disfruta de tu ETH :)")
+            else:
+                update.message.reply_text("Transacción fallida, ¿estás seguro que no tienes tu ETH diario ya?")
+        elif response.__contains__("0x"):
+            update.message.reply_text("¿Me estás vacilando? Escribe tu dirección completa si no te meto en la lista negra")
         else:
-            update.message.reply_text("Transacción fallida, ¿estás seguro que no tienes tu ETH diario ya?")
-    elif response.__contains__("0x"):
-        update.message.reply_text("¿Me estás vacilando? Escribe tu dirección completa si no te meto en la lista negra")
+            update.message.reply_text(response)
     else:
-        update.message.reply_text(response)
-    #####else:
-        #####keyboard = [[InlineKeyboardButton(text='Unete al grupo', url='https://t.me/joinchat/Wr0jQzAWqaw4Yjc8')]]
-        #####reply_markup = InlineKeyboardMarkup(keyboard)
-        #####update.message.reply_text("Para usar este bot debes estar en el grupo de @EscuelaCryptoES", reply_markup = reply_markup)
+        keyboard = [[InlineKeyboardButton(text='Unete al grupo', url='https://t.me/joinchat/Wr0jQzAWqaw4Yjc8')]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text("Para usar este bot debes estar en el grupo de @EscuelaCryptoES", reply_markup = reply_markup)
 
 def error(update,context):
     print(f"Update {update} caused error {context.error}")
